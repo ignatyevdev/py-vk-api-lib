67fd7de9ed66d00220558fee56ce6d5a7c6175c2
Maksim Ignatyev <30651393+whysqueixxet@users.noreply.github.com>
2018-10-04 21:21:54 +1000

    fix reserved identifiers

--- api/api.py
+++ api/api.py
@@ -1,7 +1,7 @@
 import urllib.request, urllib.parse
 import json
 
-from vk_api.error import Error
+from vk_api.vkerror import VKError
 from vk_api.object import VKObject
 
 def call_method(method_name, **params):
@@ -14,15 +14,15 @@ def call_method(method_name, **params):
 
     if 'error' in result:
         error = result['error']
-        raise Error(error['error_code'], error['error_msg'], error['request_params'])
+        raise VKError(error['error_code'], error['error_msg'], error['request_params'])
 
     if isinstance(result['response'], dict):
         return VKObject(result['response'])
 
     elif isinstance(result['response'], (list, tuple)):
         objects = []
-        for object in result['response']:
-            objects.append(VKObject(object))
+        for item in result['response']:
+            objects.append(VKObject(item))
         return objects
 
     return result['response']
