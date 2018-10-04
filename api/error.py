742c11554afbc0949712ca6ae9d013d455749618
Maksim Ignatyev <30651393+whysqueixxet@users.noreply.github.com>
2018-10-04 19:34:39 +1000

    Create error.py

---
+++ api/error.py
@@ -0,0 +1,7 @@
+class Error(Exception):
+
+    def __init__(self, code, message, request_params):
+        super().__init__(self, message)
+        self.code = code
+        self.message = message
+        self.request_params = request_params
